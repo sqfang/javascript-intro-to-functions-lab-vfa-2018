5fe57bf3b19d5bb48a821a60ee8e6ddb357e84e9
Sophia Fang <sophiaqfang@gmail.com>
2018-06-27 03:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string.toLowerCase()===string){
-    return "";
+    return "I can't hear you!";
+  } else if (string.toUpperCase===string){
+    
   }
 }
