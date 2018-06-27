3dd029ecc56aeb23368165254d21fac6562b3f4e
Sophia Fang <sophiaqfang@gmail.com>
2018-06-27 03:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   } else if (string.toUpperCase===string){
     return "YES INDEED!";
-  } else if (){
+  } else if (string==="I love you"){
     return "I love you, too";
   }
 }
